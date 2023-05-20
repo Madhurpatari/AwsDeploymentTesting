31bfe86a540f02bea7dfb32e8c27b9569d290a66
Madhur <kmadhur07@gmail.com>
2023-05-21 02:24:55 +0530

    using mysql

--- src/main/java/com/Geekster/AwsDeploymentTesting/Model/Employee.java
+++ src/main/java/com/Geekster/AwsDeploymentTesting/Model/Employee.java
@@ -5,12 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer employeeId;
     private String name;
     private Integer age;
